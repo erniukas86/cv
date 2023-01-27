5dbf16211c5ee6f77fdf9bb5c87fe5a18d9d0bd9
Ernestas Burokas <ernestas.burokas@presentconnection.eu>
2023-01-27 14:52:28 +0200

    asdads

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-  parser: "babel-eslint",
   env: {
     es6: true,
     node: true,
-    browser: true
+    browser: true,
   },
   parserOptions: {
     ecmaVersion: 6,
     sourceType: "module",
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   plugins: ["react"],
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:prettier/recommended"
-  ]
+    "plugin:prettier/recommended",
+  ],
 };
